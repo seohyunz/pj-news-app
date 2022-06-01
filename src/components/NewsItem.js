2bab6552d38976f93bcd6c80b2c2adcf70c500c5
seohyunz <joseohyun0201@gmail.com>
2022-06-02 08:44:08 +0900

    newsapp

---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import style from './NewsItem.module.css'
+import styled from 'styled-components'
+
+const CircledImage = styled.img`
+    border: 2px solid red;
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+`
+
+const NewsItem = function(props) {
+    const { title, description, url, urlToImage } = props.article
+    
+    return (
+        <div>
+            <h1 className={style.title}><a className={style.titleLink} href={url} target='_blank'>{title}</a></h1>
+            <CircledImage src={urlToImage} />
+            <p>{description}</p>
+        </div>
+    )
+}
+
+export default NewsItem
